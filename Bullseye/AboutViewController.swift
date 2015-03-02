e0754e67bb594a9d91962ba8aacc3acabebff114
Ray Smith <smith.rtj@gmail.com>
2015-03-02 02:17:36 +0000

    Almost done

---
+++ Bullseye/AboutViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AboutViewController.swift
+//  Bullseye
+//
+//  Created by raymond smith on 20/02/2015.
+//  Copyright (c) 2015 raymond smith. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
